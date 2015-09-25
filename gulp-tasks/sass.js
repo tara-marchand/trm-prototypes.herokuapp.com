134467720f97d827abdd72b445c8fdf00b4e09a8
Tara Marchand <tara@mac.com>
2015-09-24 23:20:46 -0700

    Moved SCSS out of CSS dir; added normalize.css

--- gulp-tasks/sass.js
+++ gulp-tasks/sass.js
@@ -2,12 +2,12 @@
 
 module.exports = function (gulp, gulpPlugins, modules, config) {
     return function () {
-        gulp.src('./app/public/stylesheets/*.scss')
+        gulp.src('./scss/*.scss')
             .pipe(gulpPlugins.sass())
             .pipe(gulpPlugins.autoprefixer({
                 browsers: ['last 2 versions'],
                 cascade: false
             }))
-            .pipe(gulp.dest('./app/public/stylesheets'));
+            .pipe(gulp.dest('./app/public/styles'));
     };
 };
