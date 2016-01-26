65c0bd4a6be59e703af50551390b96b4a5c5629d
Tara Marchand <tara@mac.com>
2016-01-25 21:19:02 -0800

    Starting to replace env vars from file

--- app/public/scripts/contractors/app-src/ContractorCollection.js
+++ app/public/scripts/contractors/app-src/ContractorCollection.js
@@ -4,7 +4,7 @@ var Contractor = require('./Contractor.js');
 
 // create a Firebase collection and set the 'url' property to the URL of our Firebase app
 module.exports = Backbone.Firebase.Collection.extend({
-    url: 'https://tmarchand-contractors.firebaseio.com/contractors',
+    url: process.env.CONTRACTORS_FIREBASE_URL,
     model: Contractor,
     autoSync: false
 });
--- app/public/scripts/contractors/contractors-app.js
+++ app/public/scripts/contractors/contractors-app.js
@@ -4,7 +4,7 @@
     function s(o, u) {
         if (!n[o]) {
             if (!t[o]) {
-                var a = typeof require == "function" && require;if (!u && a) return a(o, !0);if (i) return i(o, !0);var f = new Error("Cannot find module '" + o + "'");throw (f.code = "MODULE_NOT_FOUND", f);
+                var a = typeof require == "function" && require;if (!u && a) return a(o, !0);if (i) return i(o, !0);var f = new Error("Cannot find module '" + o + "'");throw f.code = "MODULE_NOT_FOUND", f;
             }var l = n[o] = { exports: {} };t[o][0].call(l.exports, function (e) {
                 var n = t[o][1][e];return s(n ? n : e);
             }, l, l.exports, e, t, n, r);
@@ -80,17 +80,19 @@
 
         module.exports = Contractor;
     }, {}], 3: [function (require, module, exports) {
-        'use strict';
+        (function (process) {
+            'use strict';
 
-        var Contractor = require('./Contractor.js');
+            var Contractor = require('./Contractor.js');
 
-        // create a Firebase collection and set the 'url' property to the URL of our Firebase app
-        module.exports = Backbone.Firebase.Collection.extend({
-            url: 'https://tmarchand-contractors.firebaseio.com/contractors',
-            model: Contractor,
-            autoSync: false
-        });
-    }, { "./Contractor.js": 2 }], 4: [function (require, module, exports) {
+            // create a Firebase collection and set the 'url' property to the URL of our Firebase app
+            module.exports = Backbone.Firebase.Collection.extend({
+                url: process.env.CONTRACTORS_FIREBASE_URL,
+                model: Contractor,
+                autoSync: false
+            });
+        }).call(this, require('_process'));
+    }, { "./Contractor.js": 2, "_process": 6 }], 4: [function (require, module, exports) {
         'use strict';
 
         var Contractor = require('./Contractor.js');
@@ -181,11 +183,107 @@
         var ContractorCollection = require('./ContractorCollection.js');
         var AppView = require('./AppView.js');
 
-        module.exports = (function () {
+        module.exports = function () {
             var Contractors = {};
 
             Contractors.contractorCollection = new ContractorCollection();
             Contractors.appView = new AppView({ collection: Contractors.contractorCollection });
-        })();
-    }, { "./AppView.js": 1, "./ContractorCollection.js": 3 }] }, {}, [5]);
+        }();
+    }, { "./AppView.js": 1, "./ContractorCollection.js": 3 }], 6: [function (require, module, exports) {
+        // shim for using process in browser
+
+        var process = module.exports = {};
+        var queue = [];
+        var draining = false;
+        var currentQueue;
+        var queueIndex = -1;
+
+        function cleanUpNextTick() {
+            draining = false;
+            if (currentQueue.length) {
+                queue = currentQueue.concat(queue);
+            } else {
+                queueIndex = -1;
+            }
+            if (queue.length) {
+                drainQueue();
+            }
+        }
+
+        function drainQueue() {
+            if (draining) {
+                return;
+            }
+            var timeout = setTimeout(cleanUpNextTick);
+            draining = true;
+
+            var len = queue.length;
+            while (len) {
+                currentQueue = queue;
+                queue = [];
+                while (++queueIndex < len) {
+                    if (currentQueue) {
+                        currentQueue[queueIndex].run();
+                    }
+                }
+                queueIndex = -1;
+                len = queue.length;
+            }
+            currentQueue = null;
+            draining = false;
+            clearTimeout(timeout);
+        }
+
+        process.nextTick = function (fun) {
+            var args = new Array(arguments.length - 1);
+            if (arguments.length > 1) {
+                for (var i = 1; i < arguments.length; i++) {
+                    args[i - 1] = arguments[i];
+                }
+            }
+            queue.push(new Item(fun, args));
+            if (queue.length === 1 && !draining) {
+                setTimeout(drainQueue, 0);
+            }
+        };
+
+        // v8 likes predictible objects
+        function Item(fun, array) {
+            this.fun = fun;
+            this.array = array;
+        }
+        Item.prototype.run = function () {
+            this.fun.apply(null, this.array);
+        };
+        process.title = 'browser';
+        process.browser = true;
+        process.env = {};
+        process.argv = [];
+        process.version = ''; // empty string to avoid regexp issues
+        process.versions = {};
+
+        function noop() {}
+
+        process.on = noop;
+        process.addListener = noop;
+        process.once = noop;
+        process.off = noop;
+        process.removeListener = noop;
+        process.removeAllListeners = noop;
+        process.emit = noop;
+
+        process.binding = function (name) {
+            throw new Error('process.binding is not supported');
+        };
+
+        process.cwd = function () {
+            return '/';
+        };
+        process.chdir = function (dir) {
+            throw new Error('process.chdir is not supported');
+        };
+        process.umask = function () {
+            return 0;
+        };
+    }, {}] }, {}, [5]);
 //# sourceMappingURL=contractors-app.js.map
